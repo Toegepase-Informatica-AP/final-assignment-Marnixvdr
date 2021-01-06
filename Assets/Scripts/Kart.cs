6d058f43d4d3d2eae3220d1d3dc09a9e9f3ffd86
Marnixvdr <32701626+Marnixvdr@users.noreply.github.com>
2021-01-07 00:37:26 +0100

    test

--- Assets/Scripts/Kart.cs
+++ Assets/Scripts/Kart.cs
@@ -7,6 +7,8 @@ public class Kart : Agent
 
    private KartController _kartController;
    private string LastCheckpoint;
+    private string LastCheckpoint2;
+    private string LastCheckpoint3;
     public Transform ResetPoint = null;
 
 
@@ -34,15 +36,37 @@ public class Kart : Agent
       public override void Heuristic(in ActionBuffers actionsOut)
       {
         var action = actionsOut.ContinuousActions;
-        action[0] = Input.GetAxis("Horizontal");
-        action[1] = Input.GetAxis("Vertical");
+        action[0] = 0f;  //Input.GetAxis("Horizontal");
+        action[1] = 0f;   //Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            action[1] = 1f;//
+            AddReward(0.001f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            action[1] = -1f;//
+            AddReward(-0.01f);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            action[0] = -1f; //
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            action[0] = 1f;
+
+        }
+
+
         //action[1] = Input.GetKey(KeyCode.W) ? 1f : 0f;
 
         /*if (_kartController.currentSpeed > 0f || _kartController.speed > 0f)
         {
             EndEpisode();
         }*/
-       }
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -54,8 +78,8 @@ public class Kart : Agent
         }
         else if (collision.name == LastCheckpoint)
         {
-            //AddReward(-1f);
-            //EndEpisode();
+            AddReward(-1f);
+            EndEpisode();
         }
         Debug.Log(GetCumulativeReward().ToString("f2"));
     }
