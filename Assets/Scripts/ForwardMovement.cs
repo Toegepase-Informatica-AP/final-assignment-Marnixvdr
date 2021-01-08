bdec023f02557a3111803e895f206733ec2156e8
Marnixvdr <32701626+Marnixvdr@users.noreply.github.com>
2021-01-08 02:33:00 +0100

    upd

---
+++ Assets/Scripts/ForwardMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ForwardMovement : MonoBehaviour
+{
+    private InputDevice inputSource;
+    private newMovement _movement;
+    private List<InputDevice> devices = new List<InputDevice>();
+    Rigidbody rbKart;
+
+    void Start()
+    {
+        _movement = GetComponent<newMovement>();
+
+        InputDevices.GetDevices(devices);
+        InputDeviceCharacteristics rightCh = InputDeviceCharacteristics.Right;
+        InputDevices.GetDevicesWithCharacteristics(rightCh, devices);
+        inputSource = devices.FirstOrDefault();
+
+        rbKart = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (inputSource.TryGetFeatureValue(CommonUsages.primaryButton, out bool buttonPressed) && buttonPressed)
+        {
+            _movement.ApplyAcceleration(1f);
+        }
+    }
+}
