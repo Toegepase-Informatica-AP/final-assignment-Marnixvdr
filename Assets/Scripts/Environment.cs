6e6001f4fa3ee7d7ecd2336532fb62c94f53d484
AzdineElJattari <azdine.eljattari@hotmail.com>
2021-01-16 18:24:54 +0100

    Karts verwijderd - Kleuren gegeven aan de 3 karts - Verandering in Kart.cs & Environment.cs

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -1,22 +1,23 @@
 ﻿
 using TMPro;
-using System.Collections;
 using UnityEngine;
 
 public class Environment : MonoBehaviour
 {
     private TextMeshPro scoreBoard;
-
+    private TextMeshPro lapCounter;
     private Kart kart;
 
     public void OnEnable()
     {
         kart = transform.GetComponentInChildren<Kart>();
         scoreBoard = transform.GetComponentInChildren<TextMeshPro>();
+        lapCounter = GameObject.FindWithTag("Finishlinetext").GetComponentInChildren<TextMeshPro>();
     }
 
     private void FixedUpdate()
     {
         scoreBoard.text = kart.GetCumulativeReward().ToString("f2");
+        lapCounter.text = kart.lapCounterText.ToString();
     }
 }
