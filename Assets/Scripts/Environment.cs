5cba941a1b90b4bf4002d4419365d8e3e1dfa2fc
aymane abdellah <aymaneabdellah@outlook.com>
2021-01-17 08:25:40 +0100

    bunch of changes

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -6,8 +6,39 @@ using UnityEngine;
 public class Environment : MonoBehaviour
 {
     private TextMeshPro scoreBoard;
+    public TextMeshPro countDownTimer;
+
+
 
     private Kart kart;
+
+    void Start()
+    {
+        StartCoroutine(Countdown(3));
+    }
+
+    IEnumerator Countdown(int seconds)
+    {
+        int count = seconds;
+
+        while (count >= 0)
+        {
+            countDownTimer.text = count.ToString();
+            Debug.Log(count.ToString());
+            // display something...
+            yield return new WaitForSeconds(1);
+            count--;
+        }
+
+        // count down is finished...
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        countDownTimer.text = "GO";
+    }
+
 
     public void OnEnable()
     {
